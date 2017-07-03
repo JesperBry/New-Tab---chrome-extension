3b39d1ddc64ede053f37fc06fe2ce9913b8900f8
JesperBry <jesper_bry@hotmail.com>
2017-07-03 21:38:02 +0200

    Add files via upload
    Add "0" to date < 10

--- clock.js
+++ clock.js
@@ -10,13 +10,21 @@ function startTime() {
 
   var days = ['SUN', 'MON', 'TUE', 'WED', 'THE', 'FRI', 'SAT'];
   var currWeekDay = days[today.getDay()];
-  var currDay = today.getDate();
+  var d = today.getDate();
+  d = currDay(d);
   var spaceing = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 
-  document.getElementById('date').innerHTML =  currDay.toString().bold() + spaceing + currWeekDay.toString().bold();
+  document.getElementById('date').innerHTML =  d.toString().bold() + spaceing + currWeekDay.toString().bold();
   document.getElementById('line').innerHTML = "|";
 
   var t = setTimeout(function(){ startTime() }, 500);
+}
+
+function currDay(y) {
+  if (y < 10) {
+    y = "0" + y;
+  }
+  return y;
 }
 
 function checkHours(j) {
@@ -35,15 +43,15 @@ function checkTime(i) {
 
 function getText(j) {
   if (j >= 6 && j < 12) {
-    document.getElementById('dailyText').innerHTML = "Good morning, --Name--.";
+    document.getElementById('dailyText').innerHTML = "Good morning, Jesper.";
   }
   if (j >= 12 && j <= 18) {
-    document.getElementById('dailyText').innerHTML = "Good afternoon, --Name--.";
+    document.getElementById('dailyText').innerHTML = "Good afternoon, Jesper.";
   }
   if (j > 18 && j <= 23) {
-    document.getElementById('dailyText').innerHTML = "Good evening, --Name--.";
+    document.getElementById('dailyText').innerHTML = "Good evening, Jesper.";
   }
   if (j >= 0 && j < 6) {
-    document.getElementById('dailyText').innerHTML = "Good night, --Name--.";
+    document.getElementById('dailyText').innerHTML = "Good night, Jesper.";
   }
 }
